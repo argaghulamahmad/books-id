7e24f6ac28e5f62552a82ee81126459b69d49f31
Arga Ghulam Ahmad <argaghulamahmad@gmail.com>
2019-11-13 12:30:36 +0000

    Initial Readme Add these informations to README * [x]  Team Members * [x]  Task Assignments * [x]  Requirements * [x]  How To Run

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+echo "Create books database"
 POD_NAME=$(kubectl get pod -l service=postgres -o jsonpath="{.items[0].metadata.name}")
 kubectl exec $POD_NAME --stdin --tty -- createdb -U sample books
 
 FLASK_POD_NAME=$(kubectl get pod -l app=flask -o jsonpath="{.items[0].metadata.name}")
+echo "Migrate database"
 kubectl exec $FLASK_POD_NAME --stdin --tty -- python manage.py recreate_db
+echo "Seeding database"
 kubectl exec $FLASK_POD_NAME --stdin --tty -- python manage.py seed_db
